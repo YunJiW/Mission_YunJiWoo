ea1d9a17ce900bca978ae2058c0460c1a472ba37
YunJiW <dtroy371@gmail.com>
2023-04-08 18:53:29 +0900

    refactor: delete post 변환 , yml에 민감정보삭제

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -62,8 +62,9 @@ public class LikeablePersonController {
         return "usr/likeablePerson/list";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteLikePerson(@PathVariable("id")Integer id){
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/delete/{id}")
+    public String deleteLikePerson(@PathVariable("id")Long id){
         LikeablePerson likeablePerson = this.likeablePersonService.FindById(id).orElse(null);
         if(likeablePerson == null)
             return rq.historyBack("이미 취소되었습니다.");
