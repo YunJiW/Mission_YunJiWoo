ba5c159db2b5b27b24e0ac7a976854f33044082b
YunJiW <dtroy371@gmail.com>
2023-05-02 09:37:29 +0900

    쿨타임 수정

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -109,7 +109,7 @@ public class LikeablePersonService {
     public RsData delete(LikeablePerson likeablePerson){
 
         long diff = ChronoUnit.SECONDS.between(likeablePerson.getModifyDate(), LocalDateTime.now());
-        if(diff < 10800)
+        if(diff < AppConfig.getLikeablePersonModifyCoolTime())
             return RsData.of("F-5","호감표시를 하고 3시간 이내에 삭제가 불가능합니다.");
 
         likeablePerson.getToInstaMember().decreaseLikesCount(likeablePerson.getFromInstaMember().getGender(),likeablePerson.getAttractiveTypeCode());
@@ -151,7 +151,7 @@ public class LikeablePersonService {
             return canModifyRsData;
         }
         long diff = ChronoUnit.SECONDS.between(likeablePerson.getModifyDate(), LocalDateTime.now());
-        if(diff < 10800)
+        if(diff < AppConfig.getLikeablePersonModifyCoolTime())
             return RsData.of("F-5","호감표시를 하고 3시간 이내에 수정이 불가능합니다.");
 
         likeablePerson.modifyAttractiveType(attractiveTypeCode);
