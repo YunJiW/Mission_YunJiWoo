89fe812cbebb65a7e9fa2b595fcd6b08da07b4eb
YunJiW <dtroy371@gmail.com>
2023-05-04 15:31:14 +0900

    호감표시 ë°feat :  생한후 알림 메세지 생성

--- src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
@@ -5,6 +5,8 @@ import com.ll.gramgram.base.event.EventAfterLike;
 import com.ll.gramgram.base.event.EventAfterModifyAttractiveType;
 import com.ll.gramgram.base.event.EventBeforeCancelLike;
 import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -16,17 +18,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Slf4j
 public class NotificationEventListener {
-    private final InstaMemberService instaMemberService;
+    private final NotificationService notificationService;
 
     @EventListener
     public void listen(EventAfterModifyAttractiveType event) {
         //누군가의 호감사유가 변경되었을 경우
+
     }
 
     @EventListener
     public void listen(EventAfterLike event) {
         //누군가의 호감표시를 한 경우
-        
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+
+        notificationService.makeLike(likeablePerson);
     }
     
 }
